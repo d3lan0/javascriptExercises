dd0845796bfc1c8ef03b3bf5e1578574fb1757bb
Theo.Higgins <theo.higgins1@gmail.com>
2021-10-24 10:30:32 -0400

    add logic to removeFromArray.js and enable all tests in spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
 
+      for (let i = 0; i <= args.length; i++){
+          let index;
+
+          do{
+            index = arr.indexOf(args[i])
+            if (index !== -1)
+              arr.splice(index,1);
+              continue;
+            }
+          while (index !== -1)
+      }
+    return arr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
