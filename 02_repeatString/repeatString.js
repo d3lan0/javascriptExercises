0be7f2f6e193b903e781cafa5a56109a98e90faa
Theo.Higgins <theo.higgins1@gmail.com>
2021-10-23 21:35:17 -0400

    add repeatString() in repeatString.js and remove skip tests in spec

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = '';
+    if (num >= 0){
+        for (let i = 0; i < num; i++){
+            output = output + string;
+        }
+        return output;
+    }else return "ERROR";
 };
 
 // Do not edit below this line
