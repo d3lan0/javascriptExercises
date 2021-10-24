0dd3d62dc15f7b603529df6b105a433254060136
Theo.Higgins <theo.higgins1@gmail.com>
2021-10-23 22:05:09 -0400

    Add reverseString()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
 const reverseString = function(str) {
-
+    str = str.split('');
+    let rev = [];
+    for (let i = str.length - 1; i >= 0 ; i--){
+        rev.push(str[i]);
+    }
+    return rev.join('');
 };
+
+//console.log(reverseString('test'));
 
 // Do not edit below this line
 module.exports = reverseString;
