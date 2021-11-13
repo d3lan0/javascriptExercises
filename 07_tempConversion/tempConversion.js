ecfc2d329968ab7c0ab9c733e7305428b1cf5666
Theo.Higgins <theo.higgins1@gmail.com>
2021-11-13 12:35:54 -0500

    add ignore hmtl files, add leapYears.spec, tempConversion.js and .spec

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(celc) {
+    let fahr = ((celc) - 32) * 5 / 9;
+    if (fahr % 1 === 0){
+      return fahr;
+    } else {
+      return +(Math.round(fahr *100)/100).toFixed(1);
+    }
 };
 
-const ctof = function() {
-
-};
+const ctof = function(fahr) {
+  let cel =fahr * (9/5) + 32; 
+  if (cel % 1 === 0){
+    return cel;
+  } else {
+  return +(Math.round(cel * 100) / 100).toFixed(1);
+  }
+}
 
 // Do not edit below this line
 module.exports = {
