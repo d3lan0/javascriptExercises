13847e3923c1fe507dd1185b12b1c3744d80dd22
Theo.Higgins <theo.higgins1@gmail.com>
2021-11-12 22:30:45 -0500

    add sumAll function sumAll spec

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+function sumAll(intA, intB) {
+    let sum= 0;
+    let biggerNum = 0;
+    let smallerNum = 0;
 
+    //compares numbers and sets them in bigger and smaller number variables. 
+   if (intA > intB){
+     biggerNum = intA;
+     smallerNum = intB;
+    }else{
+     biggerNum = intB;
+     smallerNum = intA;
+    }
+    //checks numbers for Error condtions
+   if (isNaN(biggerNum) || isNaN(smallerNum) || smallerNum < 0 || typeof(biggerNum) === 'string' || typeof(smallerNum) === 'string') {
+          return "ERROR";
+    }else{
+        //does calculation.
+        for (let i = smallerNum; i <= biggerNum; i++){
+            sum += i;
+        }
+        return sum;
+     }   
 };
 
 // Do not edit below this line
