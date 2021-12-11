497caa6953792a7008dca108072fe5e7a41b087a
Theo.Higgins <theo.higgins1@gmail.com>
2021-12-10 20:09:14 -0500

    add calculator function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let sum = num1+num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let diff = num1 - num2;
+  return diff;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = arr.reduce(((a,b) => a+b),0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = arr.reduce(((a,b) => a * b),1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let pow = a ** b //arr.reduce(((a,b) => a - b),1);
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++){
+      fact = i * fact;
+  }
+	return fact;
 };
 
 // Do not edit below this line
