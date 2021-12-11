2cca7c6400fe492e335bfd26ade0dc009ab098b8
Theo.Higgins <theo.higgins1@gmail.com>
2021-12-10 22:03:03 -0500

    add 09 palindrome.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const normLetters = Array.from(str.toLowerCase());
+    const test = normLetters.filter(a => (a >= 'a' && a <= 'z'));
 
+    let reverse = [];
+    for (let i = test.length-1 ; i >= 0 ; i--){
+        reverse.push(test[i])
+    }    
+    
+    let check = (reverse.join() === test.join())? true : false;
+    
+    return check;
 };
 
 // Do not edit below this line
